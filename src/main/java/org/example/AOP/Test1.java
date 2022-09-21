21cba600d78f2ef9b2b87f021e182c907030cd9b
NikitaLubimov <ikarus11122@gmail.com>
2022-09-21 08:29:32 +0300

    21.02 8:30

--- src/main/java/org/example/AOP/Test1.java
+++ src/main/java/org/example/AOP/Test1.java
@@ -1,4 +1,15 @@
 package org.example.AOP;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Test1 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MySpringConfig.class);
+        Library library = context.getBean("libraryBean", Library.class);
+        library.getBook();
+
+
+        context.close();
+    }
 }
+
